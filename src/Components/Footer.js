ccc018b274943ad6f8b3683f080235cfb8b7726b
Nadim Anwar <anwarnadim3@gmail.com>
2024-03-13 16:21:33 +0530

    second

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer class="bg-gray-800 text-white py-4 bottom-0 w-full fixed flex justify-between items-center">
             <div class="container mx-auto text-left ml-4">
-                <span class="text-lg "><FaGithub size="20px" /></span>
+                <a href="https://github.com/anwarnadim7/dictionary-app.git"><span class="text-lg "><FaGithub size="20px" /></span></a>
             </div>
             <div class="container mx-auto text-right mr-4">
                 <p>&copy; 2024 Dictionay. </p>
